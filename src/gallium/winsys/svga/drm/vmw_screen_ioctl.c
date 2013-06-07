18d87708e72037b9325ed0c6f5ef99502cebc96b
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2013-06-07 14:24:10 +0200

    ERESTART doesn't exist on *BSD; use EINTR instead

--- src/gallium/winsys/svga/drm/vmw_screen_ioctl.c
+++ src/gallium/winsys/svga/drm/vmw_screen_ioctl.c
@@ -66,6 +66,13 @@ struct vmw_region
  */
 #define SVGA3D_SURFACE_HINT_SCANOUT (1 << 9)
 
+#if defined(__DragonFly__) || \
+    defined(__FreeBSD__) ||   \
+    defined(__NetBSD__) ||    \
+    defined(__OpenBSD__)
+#define	ERESTART EINTR
+#endif
+
 uint32
 vmw_ioctl_context_create(struct vmw_winsys_screen *vws)
 {
