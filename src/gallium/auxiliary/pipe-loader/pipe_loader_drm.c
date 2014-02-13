9277fc811427e1cda9c6b088683c1f20cd900ecc
Jean-Sébastien Pédron <jean-sebastien@yakaz.com>
2014-02-13 18:21:27 +0100

    Use libdevq where libudev is required on Linux

--- src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
+++ src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
@@ -32,7 +32,14 @@
 
 #include <fcntl.h>
 #include <stdio.h>
+
+#if defined(HAVE_LIBUDEV)
 #include <libudev.h>
+#elif defined(HAVE_LIBDEVQ)
+#include <libdevq.h>
+#include <stdlib.h>
+#endif
+
 #include <xf86drm.h>
 
 #ifdef HAVE_PIPE_LOADER_XCB
@@ -62,6 +69,7 @@ struct pipe_loader_drm_device {
 static boolean
 find_drm_pci_id(struct pipe_loader_drm_device *ddev)
 {
+#if defined(HAVE_LIBUDEV)
    struct udev *udev = NULL;
    struct udev_device *parent, *device = NULL;
    struct stat stat;
@@ -100,6 +108,17 @@ find_drm_pci_id(struct pipe_loader_drm_device *ddev)
       udev_unref(udev);
 
    return FALSE;
+#elif defined(HAVE_LIBDEVQ)
+   int ret;
+
+   ret = devq_device_get_pciid_from_fd(ddev->fd,
+       &ddev->base.u.pci.vendor_id,
+       &ddev->base.u.pci.chip_id);
+   if (ret < 0)
+      return FALSE;
+
+   return TRUE;
+#endif
 }
 
 static boolean
